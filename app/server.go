ce9b77b04f2266939e294e94c8c0de2e65fdef3e
Guillermo Pineda <pinedag395@gmail.com>
2024-05-23 21:59:03 -0700

    Responding to multiple pings

--- app/server.go
+++ app/server.go
@@ -4,25 +4,50 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
 
-	fmt.Println("Waiting for connections")
+	defer listener.Close()
 
-	conn, err := l.Accept()
+	fmt.Println("Listening for connections")
+
+	for {
+		conn, err := listener.Accept()
+		fmt.Println("Connection made")
+
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		handleClient(conn)
+	}
+}
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		return
 	}
 
-	conn.Write([]byte("+PONG\r\n"))
+	fmt.Println("Received data", string(buf[:n]))
+
+	commands := strings.Fields(string(buf[:n]))
+
+	for _, command := range commands {
+		if strings.Contains(command, "PING") {
+			conn.Write([]byte("+PONG\r\n"))
+		}
+	}
 }
